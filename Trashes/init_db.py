6e18d176f6ca0a42f3fba2dfb722cbf09e138a1d
emirbalic <emirbalic@gmail.com>
2024-11-21 16:56:44 +0100

    rollback

---
+++ Trashes/init_db.py
@@ -0,0 +1,22 @@
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+#
+# # Initialize SQLAlchemy
+# db = SQLAlchemy()
+#
+# def create_app():
+#     app = Flask(__name__)
+#     app.config.from_object('config.Config')
+#
+#     # Initialize extensions
+#     db.init_app(app)
+#
+#     # Import and register routes
+#     with app.app_context():
+#         from .routes import initialize_routes
+#         initialize_routes(app)
+#
+#         # Create the database tables if they don't exist
+#         db.create_all()
+#
+#     return app
--- app/models.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from . import db
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
